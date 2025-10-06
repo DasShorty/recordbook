0bcae39e0e709a4ac26f982a8fb42800646147c4
Anthony Timmel <anthony.timmel@dasshorty.de>
2025-10-06 14:23:21 +0200

    Add Spring @Repository annotation to CompanyRepository and UserRepository interfaces

--- backend/src/main/java/de/dasshorty/recordbook/user/UserRepository.java
+++ backend/src/main/java/de/dasshorty/recordbook/user/UserRepository.java
@@ -4,11 +4,13 @@ import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends CrudRepository<UserDto, UUID> {
 
     Optional<UserDto> findByEmail(String email);
