8efd85a995ef2e3e0a25a69e923196bda245d96c
Anthony Timmel <anthony.timmel@dasshorty.de>
2025-10-16 07:04:58 +0200

    Refactor authentication controller to streamline login and refresh token methods, add endpoint for current user retrieval, and improve cookie handling

--- backend/src/main/java/de/dasshorty/recordbook/authentication/AuthenticationController.java
+++ backend/src/main/java/de/dasshorty/recordbook/authentication/AuthenticationController.java
@@ -21,8 +21,8 @@ import java.util.Optional;
 @RequestMapping("/authentication")
 public class AuthenticationController {
 
-    private final String SAME_SITE = "Lax";
     private final AuthenticationService authenticationService;
+
     @Value("${http.cookie.secure}")
     boolean secureHttpCookies;
     @Value("${http.cookie.accessToken.lifespan}")
@@ -41,16 +41,11 @@ public class AuthenticationController {
     public ResponseEntity<?> login(@RequestBody AuthenticationBody body, HttpServletResponse response) {
 
         try {
+
             Pair<String, String> tokenPair = this.authenticationService.authenticate(body.email(), body.password());
             this.setTokenCookies(response, tokenPair.getFirst(), tokenPair.getSecond());
 
-            Optional<UserDto> optional = this.authenticationService.obtainUserByToken(tokenPair.getSecond());
-
-            if (optional.isEmpty()) {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResult("account no found", "no matching id in refreshToken"));
-            }
-
-            return ResponseEntity.ok().body(optional.get().transformToBody());
+            return ResponseEntity.ok().build();
         } catch (BadCredentialsException e) {
             return ResponseEntity.badRequest().body(new ErrorResult("bad credentials", "req-body"));
         } catch (AccountNotFoundException e) {
@@ -60,7 +55,8 @@ public class AuthenticationController {
     }
 
     @GetMapping("/refresh")
-    public ResponseEntity<?> refreshAccessToken(@CookieValue(name = "refresh_token", required = false) String refreshToken, HttpServletResponse response) {
+    public ResponseEntity<?> refreshAccessToken(@CookieValue(name = "refresh_token", required = false) String refreshToken,
+                                                HttpServletResponse response) {
 
         if (!this.authenticationService.isRefreshTokenValid(refreshToken)) {
             return ResponseEntity.badRequest().body("Invalid refresh token");
@@ -76,7 +72,19 @@ public class AuthenticationController {
         String accessToken = this.authenticationService.obtainAccessToken(userDto);
 
         this.setTokenCookies(response, accessToken, refreshToken);
-        return ResponseEntity.ok().body(userDto.transformToBody());
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<?> getCurrentUser(@CookieValue(name = "access_token") String accessToken) {
+
+        Optional<UserDto> optional = this.authenticationService.obtainUserByToken(accessToken);
+
+        if (optional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResult("account not found", "access_token cookie"));
+        }
+
+        return ResponseEntity.ok(optional.get().transformToBody());
     }
 
     @PostMapping("/logout")
@@ -93,13 +101,10 @@ public class AuthenticationController {
         this.setTokenCookies(response, "", 0L, "", 0L);
     }
 
-    private void setTokenCookies(HttpServletResponse response, String accessToken, long accessTime,
-                                 String refreshToken, long refreshTime) {
+    private void setTokenCookies(HttpServletResponse response, String accessToken, long accessTime, String refreshToken, long refreshTime) {
 
-        ResponseCookie accessCookie = this.createResponseCookie("access_token", accessToken,
-                Duration.ofSeconds(accessTime));
-        ResponseCookie refreshCookie = this.createResponseCookie("refresh_token", refreshToken,
-                Duration.ofSeconds(refreshTime));
+        ResponseCookie accessCookie = this.createResponseCookie("access_token", accessToken, Duration.ofSeconds(accessTime));
+        ResponseCookie refreshCookie = this.createResponseCookie("refresh_token", refreshToken, Duration.ofSeconds(refreshTime));
 
 
         this.setCookies(response, accessCookie, refreshCookie);
@@ -107,14 +112,9 @@ public class AuthenticationController {
     }
 
     private ResponseCookie createResponseCookie(String name, String value, Duration duration) {
-        return ResponseCookie.from(name, value)
-                .httpOnly(true)
-                .secure(this.secureHttpCookies)
-                .sameSite(this.SAME_SITE)
-                .domain(this.cookieDomain)
-                .path("/")
-                .maxAge(duration)
-                .build();
+        String SAME_SITE = "Lax";
+        return ResponseCookie.from(name, value).httpOnly(true).secure(this.secureHttpCookies).sameSite(SAME_SITE).domain(this.cookieDomain)
+                .path("/").maxAge(duration).build();
     }
 
     private void setCookies(HttpServletResponse response, ResponseCookie accessCookie, ResponseCookie refreshCookie) {
