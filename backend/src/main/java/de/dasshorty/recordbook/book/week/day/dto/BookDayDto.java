d8d4a9aa5bba1f6d6a9f3645f2c825fd74ed8af6
Anthony Timmel <anthony.timmel@dasshorty.de>
2025-12-11 09:11:28 +0100

    ref: remove validation for output dto
    Signed-off-by: Anthony Timmel <anthony.timmel@dasshorty.de>

--- backend/src/main/java/de/dasshorty/recordbook/book/week/day/dto/BookDayDto.java
+++ backend/src/main/java/de/dasshorty/recordbook/book/week/day/dto/BookDayDto.java
@@ -2,8 +2,6 @@ package de.dasshorty.recordbook.book.week.day.dto;
 
 import de.dasshorty.recordbook.book.week.day.Presence;
 import de.dasshorty.recordbook.book.week.day.PresenceLocation;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 
 import java.time.LocalDate;
 import java.util.UUID;
@@ -11,11 +9,7 @@ import java.util.UUID;
 public record BookDayDto(
         UUID id,
         LocalDate date,
-        @Max(value = 24)
-        @Min(value = 0)
         int hours,
-        @Min(value = 0)
-        @Max(value = 59)
         int minutes,
         Presence presence,
         PresenceLocation presenceLocation
